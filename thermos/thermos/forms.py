fac32db6ab58dbf6e9dba58fe9c2f9e2ef76c4e5
Matt Hersh <mattmhersh@gmail.com>
2015-02-08 17:33:05 -0500

    Added jinja2 macro to dynamically add fields to the form

--- thermos/thermos/forms.py
+++ thermos/thermos/forms.py
@@ -4,5 +4,18 @@ from flask.ext.wtf.html5 import URLField
 from wtforms.validators import DataRequired, url
 
 class BookmarkForm(Form):
-    url = URLField('url', validators=[DataRequired(), url()])
-    description = StringField('description')
+    url = URLField('The URL for your bookmark:', validators=[DataRequired(), url()])
+    description = StringField('Add an optional description:')
+
+    def validate(self):
+        if not self.url.data.startswith("http://") or\
+            self.url.data.startswith("https://"):
+            self.url.data = "http://" + self.url.data
+
+        if not Form.validate(self):
+            return False
+
+        if not self.description.data:
+            self.description.data = self.url.data
+
+        return True
