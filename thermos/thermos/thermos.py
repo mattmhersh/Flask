7d44461f357547c4d0269ba94efc7e6ae642a3f7
Matt Hersh <mattmhersh@gmail.com>
2015-02-11 00:30:51 -0500

    Added SQL Lite Functionality

--- thermos/thermos/thermos.py
+++ thermos/thermos/thermos.py
@@ -1,13 +1,19 @@
+import os
 from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for
 from forms import BookmarkForm
 from logging import DEBUG
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.logger.setLevel(DEBUG)
 
 bookmarks = []
 app.config['SECRET_KEY'] = '~t\x02\xed\x187T\xc6\xa9\xfc\xe8p\x1f\xaa\xbe2R\xc4\xc5\x8a97TA?'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'thermos.db')
+db = SQLAlchemy(app)
 
 def store_bookmark(url, description):
     bookmarks.append(dict(
